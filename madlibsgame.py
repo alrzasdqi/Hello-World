13b6bd5928468818ee24914833386c16af9db07f
alrzasdqi <abaptistonz@gmail.com>
2019-04-29 20:59:23 +0430

    created a mad libs game in python

---
+++ madlibsgame.py
@@ -0,0 +1,18 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural_noun: ")
+celebrity = input("Enter a celebrity: ")
+country = input("Enter a country: ")
+city = input("Enter a city: ")
+digit = input("Enter a digit: ")
+verb = input("Enter a verb: ")
+name = input("Enter a name: ")
+
+print("Roses are " + color + ".")
+print(plural_noun + " are blue.")
+print("I love " + celebrity + ".")
+print("Once Upon a time in " + country + ".")
+print("Welcome to the " + city)
+print("We are just " + str(digit) + " lost souls.")
+print("Let us " + verb + " in a fish bowl.")
+print("Pay attention! That's what " + name + " said.")
+
