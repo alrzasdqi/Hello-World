ecd1ff31675cf915d94cd1ae7478618587e9a1f8
alrzasdqi <abaptistonz@gmail.com>
2019-04-29 18:35:19 +0430

    corrected count syntax

--- list.py
+++ list.py
@@ -1,4 +1,15 @@
+a=2
 numbers = [1,2,3,4,5,6,7,8,9]
 numbers.append(10)
+numbers.remove(a)
+numbers.pop(-3)
+numbers.pop(0)
+b=numbers.index(5)
+print("b is: ",b)
+numbers.reverse()
+numbers.sort()
 for i in numbers:
 	print(i)
+count=numbers.count(6)
+print("no.6 count is: ",count)
+
