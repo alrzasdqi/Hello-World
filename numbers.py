14dc46122577ba2a1307de7146999e6a08750800
alrzasdqi <abaptistonz@gmail.com>
2019-04-29 20:23:02 +0430

    working with numbers and math functions and import math module as well

---
+++ numbers.py
@@ -0,0 +1,24 @@
+from math import *
+
+print(2)
+print(2.098)
+print(-3.9708)
+print(3 + 4.5)
+print(3 * (4 + 5))
+print(10 % 3)
+
+my_num = -5
+print(my_num)
+
+print(str(my_num) + " is my favorite number #convert numbers into strings if you want to print out numbers along side with strings")
+
+print(abs(my_num))
+
+print(pow(4, 2))
+print(max(4, 2))
+print(min(4, 2))
+print(round(3.2))
+print(round(3.7))
+print(floor(3.2))
+print(ceil(4.1))
+print(sqrt(36))
