5fe8b22597c83a6d2e477272dec08c5bee4616b3
alrzasdqi <abaptistonz@gmail.com>
2019-04-29 20:05:03 +0430

    working more with strings here

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,13 @@
 print("Hello-World!")
+print("Giraffe\nAcademy")
+print("Giraffe\"Academy")
+phrase = "HELLO WORLD"
+print(phrase.lower() + " is cool")
+print(phrase.isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("W"))
+print(phrase.lower().index("o"))
+print(phrase.lower().index("or")) #where or starts in phrase
+print(phrase.lower().index("o"))
+print(phrase.replace("HELLO", "GOODBYE"))
