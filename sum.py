18392d79686a53d16a83fe53e60033f88250f031
alrzasdqi <abaptistonz@gmail.com>
2019-04-29 16:43:44 +0430

    vars and their types

---
+++ sum.py
@@ -0,0 +1,37 @@
+a=10
+b=15
+c=20
+
+pi=3.14
+
+cond=True
+op_cond=False
+
+message1 = 'welcome to "python world"'
+message2 = "welcome to 'python world'"
+message3 = """welcome to 'python world'
+and again welcome to "python world"!"""
+
+print("a=",a)
+print("b=",b)
+print("c=",c)
+print("pi=",pi)
+print("cond=",cond)
+print("op_cond=",op_cond)
+
+print(type(a))
+
+print("a+b=",a+b)
+print("c/a=",c/a)
+
+print("circle=",pi*c*c)
+
+print(message1)
+print(message2)
+print(message3)
+
+print("a>b is ",a>b)
+print("but a<b is ",a<b)
+
+print("True and False means: ",cond and op_cond)
+print("True OR False means: ",cond or op_cond)
