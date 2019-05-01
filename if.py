c3f41becce7232bcef8362bb8324b1294dbd6599
alrzasdqi <abaptistonz@gmail.com>
2019-05-01 17:27:41 +0430

    added some elif

--- if.py
+++ if.py
@@ -19,10 +19,14 @@ elif y:
 else:
 	print("x is true and y is false")
 #########################################################
-is_male = False
+is_male = True
 is_tall = False
 
-if is_male or is_tall:
-    print("You are a male or tall")
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and  not(is_tall):
+    print("You are a short male")
+elif not(is_male) and is_tall:
+    print("You are not a male but are tall")
 else:
-    print("You neither male nor tall")
+    print("You either not male or not tall or both")
