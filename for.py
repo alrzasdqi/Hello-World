1e52bc57b64922e11e106711ab73d2ec7654f0b8
alrzasdqi <abaptistonz@gmail.com>
2019-05-01 23:28:09 +0430

    more working with for syntax

--- for.py
+++ for.py
@@ -1,2 +1,20 @@
 for i in range(0,33,3):
 	print(i)
+##############################
+
+for letter in "Giraffe Academy":
+    print(letter)
+
+##############################
+
+friends = ["Jim", "Karen", "Kevin"]
+for friend in friends:
+    print(friend)
+
+##############################
+friends = ["Jim", "Karen", "Kevin"]
+for index in range(len(friends)):
+    print(friends[index])
+
+##############################
+
