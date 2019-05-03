c6f2bb02947255530ae7d6c4f31568204b0cd947
alrzasdqi <abaptistonz@gmail.com>
2019-05-03 19:52:29 +0430

    correction

--- class/app.py
+++ class/app.py
@@ -1,6 +1,6 @@
 from student import student
 student1 = student("Jim", "Business", 3.1, False)
-student1 = student("Pam", "Art", 2.5, True)
+student2 = student("Pam", "Art", 2.5, True)
 
 print(student1.name)
 print(student2.gpa)
